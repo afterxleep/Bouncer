8338659559e787fda597fb4a8d37f8fcb8f5a4e8
Daniel Bernal <yo@danielbernal.co>
2020-07-11 22:29:13 -0500

    Removed Prints and non required spaces

--- Bouncer/Services/RatingService/RatingServiceDefault.swift
+++ Bouncer/Services/RatingService/RatingServiceDefault.swift
@@ -13,20 +13,16 @@ struct RatingServiceStoreKit: RatingService {
     var launchesRequired: Int = 3
     let userSettings: UserSettings
     
-    
     init(userSettings: UserSettings = UserSettingsDefaults()) {
         self.userSettings = userSettings
     }
     
     func requestReview() {
-        // Get the current bundle version for the app
         let infoDictionaryKey = kCFBundleVersionKey as String
         guard let currentVersion = Bundle.main.object(forInfoDictionaryKey: infoDictionaryKey) as? String
             else { fatalError("Expected to find a bundle version in the info dictionary") }
         
         let lastVersionPromptedForReview = userSettings.lastVersionPromptedForReview
-
-        print(userSettings.numberOfLaunches)
         
         // Has the process been completed several times and the user has not already been prompted for this version?
         if userSettings.numberOfLaunches >= launchesRequired && currentVersion != lastVersionPromptedForReview {
