97e1b2fd66e1e300cebe7c09318c373a3562cd20
Daniel Bernal <danielbernal@hey.com>
2025-04-06 18:09:52 +0200

    v.2.8.0 bump

---
+++ Bouncer/Config/Config.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct AppConfig {
+    
+    /// Supabase instance URL & Key - Required for analytics
+    static let supabaseURL = "https://yqrxbdnqfdthiuegufvd.supabase.co"    
+    static let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxcnhiZG5xZmR0aGl1ZWd1ZnZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzNTQ5NDMsImV4cCI6MjA1ODkzMDk0M30.CTKgoIainJ-7WxDe-KL-DkEJE_Px023U5XR4XQL6Gdw"
+} 
